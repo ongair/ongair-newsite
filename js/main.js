df8d4a54a1d2e199f95da53cf625a6d58445685f
Michaelmwirigi <mwirigimichael@gmail.com>
2015-04-30 12:22:13 +0300

    few changes

--- js/main.js
+++ js/main.js
@@ -7,10 +7,21 @@ $(document)
 
 	$('.ui.sticky')
 	  .sticky({
-	    context: 'body'
-
+	    context: 'body',
+      pushing: true
 	  })
 	;
+  $('nav.ui.mobile_menu.grid.mobile.only .button')
+  .popup({
+    inline   : true,
+    hoverable: true,
+    position : 'bottom right',
+    delay: {
+      show: 100,
+      hide: 800
+    }
+  })
+;
 
   $('.ui.accordion')
   .accordion( 
